479f5a7d41e1cae37c1f4cd3117c0a2fb234b9aa
Zé <zepspaiva@gmail.com>
2015-12-03 22:55:37 -0300

    .

--- neuraltalk-http.js
+++ neuraltalk-http.js
@@ -314,7 +314,7 @@ function generategif(folderpath, callback) {
 
 		ext = path.extname(sub.filename);
 
-		var proc = spawn("convert", [sub.filename, '-gravity', 'south', '-stroke', "'#000C'", '-strokewidth', '2', '-annotate', '0', "'" + sub.caption + "'", '-pointsize', '30',  '-stroke', 'none', '-fill', 'white', '-annotate', '0', "'" + sub.caption + "'", '-pointsize', '30', sub.filename], { cwd: folderpath });
+		var proc = spawn("convert", [sub.filename, '-gravity', 'north', '-stroke', "'#000C'", '-strokewidth', '2', '-annotate', '0', "'" + sub.caption + "'", '-pointsize', '40',  '-stroke', 'none', '-fill', 'white', '-annotate', '0', "'" + sub.caption + "'", '-pointsize', '40', sub.filename], { cwd: folderpath });
 
 		proc.on('close', function(code) {
 
